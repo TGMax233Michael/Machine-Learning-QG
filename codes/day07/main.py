459af444eb4ccc9a0be622776e43ffee9a1fb59f
TGMax233_Michael <tgmax233michael@163.com>
2025-02-10 19:26:33 +0800

    fix issues

--- codes/day07/main.py
+++ codes/day07/main.py
@@ -1,51 +1,50 @@
-from models.regression import MyLinearRegression, MyPolynomialRegression
-from sklearn.datasets import fetch_california_housing, make_regression
+from models.regression import MyLinearRegression
+from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 from preprocessing import polynomial_features
 import pandas as pd
+import numpy as np
 
-# california_housing = fetch_california_housing(as_frame=True)
-# data = california_housing.frame
+def show_result(model, name=None):
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    if name:
+        print(name)
+    else:
+        print(model.__class__.__name__)
+        
+    print(f"MSE: {mean_squared_error(y_test, y_pred)}")
+    print(f"R2: {r2_score(y_test, y_pred)}")
+    print()
 
-# print(data.describe().T)
-# print(data.sample(5).T)
+data = pd.read_csv("BostonHousing.csv")
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+# print(X.isna().sum())
+# print(y.isna().sum())
 
-X, y = make_regression(n_samples=100, n_features=3, noise=1, random_state=42)
+# rm 存在 5 个缺失值，数量较少采取直接删除样本
+X = X.dropna()
 
-# X = data.iloc[:, :-1]
-# y = data.iloc[:, -1]
+X = np.array(X)
+y = np.array(y)
 
-# print(X)
-# print(y)
+
+X, y = make_regression(n_samples=1000, n_features=3, noise=1, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
 sklinear_model = LinearRegression()
-sklinear_model.fit(X_train, y_train)
-y_pred = sklinear_model.predict(X_test)
-print("sklearn linear")
-print(f"mse: {mean_squared_error(y_test, y_pred)}")
-print(f"r2: {r2_score(y_test, y_pred)}")
-print()
+my_linear_model = MyLinearRegression(iterations=1000, print_info=False)
+skpoly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+my_poly_model = MyLinearRegression(polynomial=True, degree=2, iterations=1000, print_info=False)
+models = [sklinear_model, my_linear_model, skpoly_model, my_poly_model]
 
-my_linear_model = MyLinearRegression(iterations=100, print_info=False)
-my_linear_model.fit(X_train, y_train)
-y_pred = my_linear_model.predict(X_test)
-print("my linear")
-print(f"mse: {mean_squared_error(y_test, y_pred)}")
-print(f"r2: {r2_score(y_test, y_pred)}")
-print()
-
-my_polynomial_model = MyPolynomialRegression(degree=2, iterations=100, print_info=False)
-my_polynomial_model.fit(X, y)
-y_pred = my_linear_model.predict(X_test)
-print("my polynomial")
-print(f"mse: {mean_squared_error(y_test, y_pred)}")
-print(f"r2: {r2_score(y_test, y_pred)}")
-print()
-
-print(polynomial_features(X, degree=2))
+for model in models:
+    show_result(model)
--- codes/day07/models/regression.py
+++ codes/day07/models/regression.py
@@ -2,23 +2,35 @@ import numpy as np
 from preprocessing import polynomial_features
 
 class MyLinearRegression:
-    def __init__(self, iterations=100, learning_rate=0.1, print_info=False):
+    def __init__(self, 
+                iterations=1000, 
+                learning_rate=0.1, 
+                print_info=False, 
+                polynomial=False,
+                degree=None,
+                adagrad=False):
         self.iterations = iterations
         self.learning_rate = learning_rate
+        self.polynomial = polynomial
+        self.print_info = print_info
+        self.degree = degree
+        self.adagrad = adagrad
         self.weight = None
         self.mse = None
-        self.print_info = print_info
-    
     
     def fit(self, X: np.ndarray, y: np.ndarray):
         if X.ndim == 1:
             X = X.reshape(-1, 1)
             
+        if self.polynomial:
+            X = polynomial_features(X, self.degree)
+            
         X = np.column_stack((np.ones(X.shape[0]), X))
             
         m, n = X.shape[0], X.shape[1]
         
-        # r = np.ones(n)
+        if self.adagrad:
+            r = np.ones(n)
         
         # 初始权重
         weight = np.random.randn(n)
@@ -29,14 +41,17 @@ class MyLinearRegression:
         for _ in range(self.iterations):
             # 计算均方误差
             y_pred = X.dot(weight)
-            mse = np.mean((y_pred - y) ** 2)            
+            mse = np.mean((y_pred - y) ** 2)
+                        
             # 梯度下降
             gradient = 2 / m * X.T.dot(y_pred - y)
             
-            weight -= gradient * self.learning_rate
-            # # adagrad 算法
-            # r += gradient ** 2
-            # weight -= gradient * (self.learning_rate / (np.sqrt(r) + 1e-8))
+            
+            if self.adagrad == False:
+                weight -= gradient * self.learning_rate
+            else:
+                r += gradient ** 2
+                weight -= gradient * (self.learning_rate / np.sqrt(r) + 1e-8)
             
             if self.print_info:
                 print(f"第{_+1}次迭代\n"
@@ -49,46 +64,7 @@ class MyLinearRegression:
         
         
     def predict(self, X):
+        if self.polynomial:
+            X = polynomial_features(X, self.degree)
+        
         return X.dot(self.weight[1:]) + self.weight[0]
-    
-class MyPolynomialRegression:
-    def __init__(self, degree = 1, iterations = 100, learning_rate = 0.1, print_info = False):
-        self.weight = None
-        self.mse = None
-        self.degree = degree
-        self.iterations = iterations
-        self.learning_rate = learning_rate
-        self.print_info = print_info
-        
-    def fit(self, X: np.ndarray, y: np.ndarray):
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-            
-        X = polynomial_features(X, self.degree)
-        X = np.column_stack((np.ones(X.shape[0]), X))
-            
-        m, n = X.shape[0], X.shape[1]
-        
-        weight = np.random.rand(n)
-        
-        if self.print_info:
-            print(f"初始权重\n{weight}\n")
-        
-        for _ in range(self.iterations):
-            y_pred = X.dot(weight)
-            mse = np.mean((y_pred - y) ** 2)
-            
-            gradient = 2/m * X.T.dot(y_pred - y)
-            
-            weight -= gradient * self.learning_rate
-            
-            if self.print_info:
-                print(f"第{_+1}次迭代\n"
-                      f"权重\n{weight}\n"
-                      f"MSE {mse}\n\n")
-            
-        self.weight = weight
-        self.mse = mse
-        
-    def predict(self, X):
-        return X.dot(self.weight[1:, :]) + self.weight[0, :]
