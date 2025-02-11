6dd0f279c0e751e27d2ab93dff33b785cffa4e49
TGMax233_Michael <tgmax233michael@163.com>
2025-02-11 15:10:19 +0800

    fix issues

--- codes/day07/main.py
+++ codes/day07/main.py
@@ -1,4 +1,4 @@
-from models.regression import MyLinearRegression
+from models.regression import MyLinearRegression, info_density
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
@@ -21,30 +21,44 @@ def show_result(model, name=None):
     print(f"R2: {r2_score(y_test, y_pred)}")
     print()
 
+
+# 1. 数据预处理
 data = pd.read_csv("BostonHousing.csv")
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
 # print(X.isna().sum())
 # print(y.isna().sum())
 
 # rm 存在 5 个缺失值，数量较少采取直接删除样本
-X = X.dropna()
-
+data = data.dropna()
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 X = np.array(X)
 y = np.array(y)
-
-
-X, y = make_regression(n_samples=1000, n_features=3, noise=1, random_state=42)
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# 2. 学习过程
 sklinear_model = LinearRegression()
-my_linear_model = MyLinearRegression(iterations=1000, print_info=False)
+my_linear_model = MyLinearRegression(
+                            learning_rate=0.001, 
+                            iterations=1000, 
+                            print_info=info_density.no,
+                            adagrad=False,
+                            mini_batch=True
+                        )
 skpoly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
-my_poly_model = MyLinearRegression(polynomial=True, degree=2, iterations=1000, print_info=False)
+my_poly_model = MyLinearRegression(
+                            learning_rate=0.001, 
+                            polynomial=True, 
+                            degree=2, 
+                            iterations=1000, 
+                            print_info=info_density.no,
+                            adagrad=True,
+                            mini_batch=True
+                        )
 models = [sklinear_model, my_linear_model, skpoly_model, my_poly_model]
 
+
+# 3. 模型评估
 for model in models:
     show_result(model)
--- codes/day07/models/regression.py
+++ codes/day07/models/regression.py
@@ -1,20 +1,32 @@
 import numpy as np
 from preprocessing import polynomial_features
+from enum import IntEnum, auto
 
+class info_density(IntEnum):
+    no = auto()
+    few = auto()
+    all = auto()
+
+# TODO 增加梯度裁剪防止梯度爆炸
 class MyLinearRegression:
     def __init__(self, 
                 iterations=1000, 
                 learning_rate=0.1, 
-                print_info=False, 
+                print_info=info_density.no,
                 polynomial=False,
                 degree=None,
-                adagrad=False):
+                adagrad=False,
+                epsilon=1e-8,
+                mini_batch=False,
+                ):
         self.iterations = iterations
         self.learning_rate = learning_rate
         self.polynomial = polynomial
         self.print_info = print_info
         self.degree = degree
         self.adagrad = adagrad
+        self.epsilon = epsilon
+        self.mini_batch = mini_batch
         self.weight = None
         self.mse = None
     
@@ -33,30 +45,40 @@ class MyLinearRegression:
             r = np.ones(n)
         
         # 初始权重
-        weight = np.random.randn(n)
+        weight = np.zeros(n)
         
-        if self.print_info:
+        if self.print_info != info_density.no:
             print(f"初始权重\n{weight}\n")
         
         for _ in range(self.iterations):
-            # 计算均方误差
-            y_pred = X.dot(weight)
-            mse = np.mean((y_pred - y) ** 2)
-                        
-            # 梯度下降
-            gradient = 2 / m * X.T.dot(y_pred - y)
+            if self.mini_batch:
+                indices = np.random.choice(m, m//10, replace=False)
+                X_batch = X[indices]
+                y_batch = y[indices]
+                y_batch_pred = X_batch.dot(weight)
+                gradient = 2 / (m//10) * X_batch.T.dot(y_batch_pred - y_batch)
+            else:
+                y_pred = X.dot(weight)
+                gradient = 2 / m * X.T.dot(y_pred - y)
             
-            
-            if self.adagrad == False:
+            if not self.adagrad:
                 weight -= gradient * self.learning_rate
             else:
                 r += gradient ** 2
-                weight -= gradient * (self.learning_rate / np.sqrt(r) + 1e-8)
+                weight -= gradient * (self.learning_rate / (np.sqrt(r) + self.epsilon))
             
-            if self.print_info:
+            y_pred_new = X.dot(weight)
+            mse = np.mean((y_pred_new - y) ** 2)
+
+            if self.print_info == info_density.few:
+                if _ % 5 == 0:
+                    print(f"第{_+1}次迭代\n"
+                        f"权重\n{weight}\n"
+                        f"MSE {mse}\n\n")
+            elif self.print_info == info_density.all:
                 print(f"第{_+1}次迭代\n"
-                      f"权重\n{weight}\n"
-                      f"MSE {mse}\n\n")
+                        f"权重\n{weight}\n"
+                        f"MSE {mse}\n\n")
             
         self.weight = weight
         self.mse = mse
@@ -64,7 +86,10 @@ class MyLinearRegression:
         
         
     def predict(self, X):
+        if X.ndim == 1:
+            X.reshape(-1, 1)
+        
         if self.polynomial:
             X = polynomial_features(X, self.degree)
         
-        return X.dot(self.weight[1:]) + self.weight[0]
+        return X.dot(self.weight[1:]) + self.weight[0]  
